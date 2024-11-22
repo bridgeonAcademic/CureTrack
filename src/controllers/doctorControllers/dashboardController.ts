6e8d511111650430eaa37fafa7f8fec22be4fa87
erfanpv <erfanpv786@gmail.com>
2024-11-22 18:19:08 +0530

    feat(doctorControllers): add auth and dashboard controllers; remove sample file

---
+++ src/controllers/doctorControllers/dashboardController.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import Users from "../../models/userModels/userSchema";
+import Doctors from "../../models/doctorModels/doctorSchema";
+import Vendors from "../../models/vendorModels/vendorsSchema";
+import sendResponse from "../../utils/handlResponse";
+
+export const getHospitalDashboardCounts = async (
+  req: Request,
+  res: Response
+): Promise<void> => {c};
